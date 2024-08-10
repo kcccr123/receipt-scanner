3f9295ef7ede18341291c3229556a50b6cceecfb
Kevin Chen <109507575+kcccr123@users.noreply.github.com>
2024-08-10 00:01:59 -0400

    image picker can now crop with no restrictions

--- app/(tabs)/(home)/index.tsx
+++ app/(tabs)/(home)/index.tsx
@@ -1,20 +1,16 @@
 import { View, Text, StyleSheet, Button } from "react-native";
 import * as ImagePicker from "expo-image-picker";
-import { runOnnxModel } from "../(camera)/utils";
 import { sayHello, detectImagePost } from "../requests";
 
 const pickImage = async () => {
   // No permissions request is necessary for launching the image library
   let result = await ImagePicker.launchImageLibraryAsync({
-    mediaTypes: ImagePicker.MediaTypeOptions.All,
+    mediaTypes: ImagePicker.MediaTypeOptions.Images,
     allowsEditing: true,
-    aspect: [4, 3],
     quality: 1,
   });
   if (!result.canceled) {
-    //sayHello("hello");
     detectImagePost(result.assets[0].uri);
-    // runOnnxModel(result.assets[0].uri);
   }
 };
 
