6f73721f6f05f5b126e98819a488f14a493fc712
Kevin Chen <109507575+kcccr123@users.noreply.github.com>
2024-09-15 12:09:51 -0400

    removed env llibrary

--- app/(tabs)/requests.ts
+++ app/(tabs)/requests.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 import mime from "mime";
 import { ProcessedReceipt } from "./types";
-import { GCP_URL_RESPONSE, GCP_URL_PREDICTION } from "@env";
 
 export const detectImagePost = async (
   uri: string
@@ -22,16 +21,25 @@ export const detectImagePost = async (
   } as any);
   console.log(formData);
   return await axios
-    .post(GCP_URL_PREDICTION, formData, {
+    .post(process.env.EXPO_PUBLIC_LOCAL_URL_PREDICTION as string, formData, {
       headers: {
         "Content-Type": "multipart/form-data",
       },
+      timeout: 120000,
     })
     .then((response) => {
       console.log(response);
       return { status: response.status, data: response.data };
     })
     .catch((error) => {
+      console.log(error);
+      if (error.response) {
+        console.log("Error response:", error.response.data);
+      } else if (error.request) {
+        console.log("No response received:", error.request);
+      } else {
+        console.log("Axios error:", error.message);
+      }
       return {
         status: error.status,
       };
@@ -41,9 +49,12 @@ export const detectImagePost = async (
 export const sayHello = async (words: string) => {
   console.log("make request");
   try {
-    const response = await axios.post(GCP_URL_RESPONSE, {
-      message: words,
-    });
+    const response = await axios.post(
+      process.env.EXPO_PUBLIC_LOCAL_URL_RESPONSE as string,
+      {
+        message: words,
+      }
+    );
     alert(response.data.response);
   } catch (error) {
     console.error(error);
--- babel.config.js
+++ babel.config.js
@@ -2,16 +2,10 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      'react-native-paper/babel',
-      [
-        'module:react-native-dotenv',
-        {
-          moduleName: '@env',
-          path: '.env',
-        },
-      ],
-    ],
-   
+    env: {
+      production: {
+        plugins: ['react-native-paper/babel'],
+      },
+    },
   };
 };
