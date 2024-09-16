384c45b7a05bb23bdcaee28bbc6c00e8fa75fbb4
Gary Guo <garyguo2004@gmail.com>
2024-09-16 13:40:01 -0400

    version 1 release

--- app/Other/requests.ts
+++ app/Other/requests.ts
@@ -1,13 +1,13 @@
 import axios from "axios";
 import mime from "mime";
 import { ProcessedReceipt } from "./types";
-import { GCP_URL_RESPONSE, GCP_URL_PREDICTION } from "@env";
 
 export const detectImagePost = async (
   uri: string
 ): Promise<{ status: number; data?: ProcessedReceipt }> => {
   //  get uri and format
   const newImageUri = "file://" + uri.split("file:/").join("");
+  console.log(newImageUri);
 
   // So, FormData object automatically converts image to binary data.
   // its given the image uri, takes the image uri, uses it to get the image and convert to binary data
@@ -19,16 +19,27 @@ export const detectImagePost = async (
     type: mime.getType(newImageUri),
     name: newImageUri.split("/").pop(),
   } as any);
+  console.log(formData);
   return await axios
-    .post(GCP_URL_PREDICTION, formData, {
+    .post(process.env.EXPO_PUBLIC_GCP_URL_PREDICTION as string, formData, {
       headers: {
         "Content-Type": "multipart/form-data",
       },
+      timeout: 120000,
     })
     .then((response) => {
+      console.log(response);
       return { status: response.status, data: response.data };
     })
     .catch((error) => {
+      console.log(error);
+      if (error.response) {
+        console.log("Error response:", error.response.data);
+      } else if (error.request) {
+        console.log("No response received:", error.request);
+      } else {
+        console.log("Axios error:", error.message);
+      }
       return {
         status: error.status,
       };
@@ -36,10 +47,14 @@ export const detectImagePost = async (
 };
 
 export const sayHello = async (words: string) => {
+  console.log("make request");
   try {
-    const response = await axios.post(GCP_URL_RESPONSE, {
-      message: words,
-    });
+    const response = await axios.post(
+      process.env.EXPO_PUBLIC_GCP_URL_RESPONSE as string,
+      {
+        message: words,
+      }
+    );
     alert(response.data.response);
   } catch (error) {
     console.error(error);
